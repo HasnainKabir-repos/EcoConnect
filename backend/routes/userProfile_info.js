ff20ff15e14e83761b50274dbc24174d0319e8fd
mamunur03 <mamunurrahman0003@gmail.com>
2023-10-14 15:24:26 +0600

    profile_backend(pic upload)
    install multer

--- backend/routes/userProfile_info.js
+++ backend/routes/userProfile_info.js
@@ -3,22 +3,48 @@ const router = express.Router();
 const { User } = require('../models/user');
 const UserProfile = require('../models/userProfile');
 const authenticate = require('../middleware/authenticate');
+const multer = require('multer');
 
+// Configure multer for file uploads
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
 
-router.post('/', authenticate, async (req, res) => {
+const upload = multer({ storage: storage });
+
+// Update user profile, including profile image
+router.post('/', authenticate, upload.single('profileImage'), async (req, res) => {
     try {
         const { email } = req.user;
         const { bio, address } = req.body;
 
-        const userProfile = {
-            useremail: email,
-            bio,
-            address
+        const profileImagePath = req.file ? req.file.path : undefined; // Check if a file was uploaded
+
+        const userProfileData = {
+            $set: {
+                useremail: email,
+                bio: bio,
+                address: address,
+            }
         };
+
+        if (profileImagePath) {
+            userProfileData.$set.profileImage = profileImagePath;
+        }
 
         const filter = { useremail: email };
         const options = { new: true, upsert: true };
-        const updatedProfile = await UserProfile.findOneAndUpdate(filter, userProfile, options);
+
+        const updatedProfile = await UserProfile.findOneAndUpdate(
+            filter,
+            userProfileData,
+            options
+        );
 
         if (updatedProfile) {
             res.status(200).json({ email: email, message: 'User Profile Updated successfully' });
@@ -26,11 +52,12 @@ router.post('/', authenticate, async (req, res) => {
             res.status(500).json({ message: 'Failed to update user profile' });
         }
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error', error: error.message });
     }
 });
 
+// Retrieve user profile
 router.get('/', authenticate, async (req, res) => {
     try {
         const { email } = req.user;
@@ -42,19 +69,25 @@ router.get('/', authenticate, async (req, res) => {
             res.status(404).json({ message: 'User profile not found' });
         }
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Error retrieving user profiles' });
+        console.error(error);
+        res.status(500).json({ message: 'Error retrieving user profile', error: error.message });
     }
 });
 
+// Retrieve user profile by email
 router.get('/find', async (req, res) => {
     try {
         const { email } = req.query;
         const userProfiles = await UserProfile.findOne({ useremail: email });
 
-        return res.json(userProfiles);
+        if (userProfiles) {
+            res.json(userProfiles);
+        } else {
+            res.status(404).json({ message: 'User profile not found' });
+        }
     } catch (error) {
-        return res.status(500).json({ message: 'Error retrieving user profiles', error: error });
+        console.error(error);
+        res.status(500).json({ message: 'Error retrieving user profile', error: error.message });
     }
 });
 
