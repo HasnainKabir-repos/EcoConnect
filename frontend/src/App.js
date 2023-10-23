e47e5b77a290f65b388d5523491c59e791ef3a5a
Mukit Mahdin <88025578+mahdin70@users.noreply.github.com>
2023-10-23 22:37:30 +0600

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import EditProfile from "./components/EditProfile";
 import ForgotPassword from "./components/ForgotPassword";
 import Login from "./components/Login";
 import Main from "./components/Main";
 import PasswordReset from "./components/PasswordReset";
-import Signup from "./components/Signup";
 import Profile from "./components/Profile";
-import EditProfile from "./components/EditProfile";
+import Signup from "./components/Signup";
 function App() {
-	const user = localStorage.getItem("token");
+  const user = localStorage.getItem("token");
 
-	return (
-		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
-			<Route path="/forgot-password" element={<ForgotPassword />} />
-			<Route path="/password-reset/:id/:token" element={<PasswordReset />} />
-      <Route path="/profile" exact element={<Profile/>}/>
-      <Route path="/editprofile" exact element={<EditProfile/>}/>
-		</Routes>
-	);
+  return (
+    <Routes>
+      {user && <Route path="/" exact element={<Main />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/" element={<Navigate replace to="/login" />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/password-reset/:id/:token" element={<PasswordReset />} />
+      <Route path="/profile" exact element={<Profile />} />
+      <Route path="/editprofile" exact element={<EditProfile />} />
+    </Routes>
+  );
 }
 
 export default App;
