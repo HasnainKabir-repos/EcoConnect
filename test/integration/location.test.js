a3eeb6bf02ae66daa02ec3a9992a52b59a5d78c1
Hasnain-iut <hasnainkabir@iut-dhaka.edu>
2023-11-12 17:02:44 +0600

    test cases

---
+++ test/integration/location.test.js
@@ -0,0 +1,33 @@
+const supertest = require("supertest");
+const app = require('../../backend/index');
+
+
+describe('Integration test location', () => {
+    it('should return 200 response when fetching autocomplete data based on a text', () => {
+        const data = {
+            input: "Shantinagar"
+        };
+
+        supertest(app)
+            .post('/api/location/search')
+            .send(data)
+            .expect(200)
+            .end((err, res) => {
+                if (err) console.log(err);
+            });
+    });
+
+    it('should return 200 response when fetching details based on a place_id', () => {
+        const data = {
+            place_id: "ChIJHWQBQ525VTcRNgbcwue5oEA",
+            description: "Shantinagar Mour, Bir Uttam Samsul Alam Road, Dhaka, Bangladesh"
+        };
+        supertest(app)
+            .post('/api/location/details')
+            .send(data)
+            .expect(200)
+            .end((err, res) => {
+                if (err) console.log(err);
+            });
+    })
+})
