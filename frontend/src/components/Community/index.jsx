38f27b582703cb16e29eff15bb78a13fa9f1b8c4
Mukit Mahdin <88025578+mahdin70@users.noreply.github.com>
2024-01-16 03:08:35 +0600

    Community Conflict Resolve

--- frontend/src/components/Community/index.jsx
+++ frontend/src/components/Community/index.jsx
@@ -125,7 +125,9 @@ const Community = () => {
     <>
       <TopBar />
       <main className="pt-20 min-h-screen min-w-screen bg-gray-200">
-        <div>{isLoading || isLoading2 ? <Loader /> : console.log("Loaded")}</div>
+        <div>
+          {isLoading || isLoading2 ? <Loader /> : console.log("Loaded")}
+        </div>
         <div className="fixed flex flex-row w-full">
           <div className="w-2/6 p-2 ml-4 mt-10 mb-2">
             <div className="flex flex-col  h-full">
@@ -134,35 +136,41 @@ const Community = () => {
                   My Communities:
                 </div>
                 <div className="flex flex-wrap flex-col">
-                  {(joinedCommunities.length > 0) ? joinedCommunities.map((community, index) => (
-                    <div
-                      key={index}
-                      className={`font-bold py-1 my-1 px-2 flex items-center rounded-full border border-teal-900 border-solid hover:border-dashed 
-              ${selectedCommunity === community
-                          ? "bg-teal-900 text-white"
-                          : "bg-white text-gray-900"
-                        }
-            `}
-                      onClick={() => setSelectedCommunity(community)}
-                    >
+                  {joinedCommunities.length > 0 ? (
+                    joinedCommunities.map((community, index) => (
                       <div
-                        className={`w-2 h-2 rounded-full mr-2 ${getRandomColor()}`}
-                      ></div>
-                      <div className="text-base">{community.name}</div>
-                    </div>
-                  ))
-                    : <h3 className="text-xl font-bold text-center mt-10">
+                        key={index}
+                        className={`font-bold py-1 my-1 px-2 flex items-center rounded-full border border-teal-900 border-solid hover:border-dashed 
+              ${
+                selectedCommunity === community
+                  ? "bg-teal-900 text-white"
+                  : "bg-white text-gray-900"
+              }
+            `}
+                        onClick={() => setSelectedCommunity(community)}
+                      >
+                        <div
+                          className={`w-2 h-2 rounded-full mr-2 ${getRandomColor()}`}
+                        ></div>
+                        <div className="text-base">{community.name}</div>
+                      </div>
+                    ))
+                  ) : (
+                    <h3 className="text-xl font-bold text-center mt-10">
                       Join a community to start
                     </h3>
-                  }
+                  )}
                 </div>
               </div>
-              <div className="font-semibold text-xl text-teal-900 mb-2 mt-6">
-                Members of {selectedCommunity.name}:
-              </div>
-              <div className="overflow-auto max-h-80 pr-2 rounded-lg border-2 border-teal-900 p-2 cursor-pointer">
+
+              <div className="overflow-auto max-h-80 bg-white rounded-lg  px-6 py-4 cursor-pointer mt-6">
+                <div className="font-semibold text-xl text-teal-900 mb-2">
+                  Members of {selectedCommunity.name}:
+                </div>
                 <div className="flex flex-wrap flex-col">
-                  {selectedCommunity != "" && selectedCommunity.members && selectedCommunity.members.length > 0 ?
+                  {selectedCommunity !== "" &&
+                  selectedCommunity.members &&
+                  selectedCommunity.members.length > 0 ? (
                     selectedCommunity.members.map((member, index) => (
                       <div
                         key={index}
@@ -170,13 +178,12 @@ const Community = () => {
                       >
                         {member}
                       </div>
-                    )) :
-                    <div
-                      className="font-bold text-base bg-teal-300 text-black rounded-full px-3 py-1 m-1"
-                    >
-                      No members in this community
+                    ))
+                  ) : (
+                    <div className="font-bold text-base bg-gray-300 text-black rounded-full px-3 py-1 m-1">
+                      No members
                     </div>
-                  }
+                  )}
                 </div>
               </div>
             </div>
@@ -184,76 +191,60 @@ const Community = () => {
 
           <div className="w-5/6 mt-8 ml-4 mr-5 pr-2 pb-36 overflow-auto max-h-screen">
             <div className="flex flex-col mx-auto p-4">
-              <div className="flex flex-col mx-4 w-full border-2 border-gray-500 rounded-lg p-2">
+              <div className="flex flex-col mx-4 w-full bg-white rounded-lg p-4">
                 <h3 className="text-lg font-bold text-center mb-4">
                   Post Something to {selectedCommunity.name}
                 </h3>
-                <form
-                  className="flex flex-col items-center w-full"
-                  
-                >
+                <form className="flex flex-col items-center w-full">
                   <textarea
                     value={postContent}
                     onChange={(e) => setPostContent(e.target.value)}
                     placeholder="Type your Post Here..."
                     className="border rounded-lg p-3 mb-3 w-5/6"
                   />
-                  <button
-                    className="font-bold px-4 py-2 rounded-full bg-black hover:bg-teal-700 text-white inline-block w-1/4"
-                    type="submit"
-                    onClick={() => {handlePostSubmit(selectedCommunity)}}
-                  >
-                    <textarea
-                      value={postContent}
-                      onChange={(e) => setPostContent(e.target.value)}
-                      placeholder="Type your Post Here..."
-                      className="border-2 rounded-lg p-3 mb-3 w-5/6"
+                  <div className="flex flex-col w-full">
+                    <label className="ml-16 mb-1 mt-3 text-md font-semibold">
+                      Attach Relevant Image (If Any):
+                    </label>
+                    <input
+                      type="file"
+                      id="image"
+                      accept="image/*"
+                      onChange={handleImageChange}
+                      className="mb-3 ml-16"
                     />
-                    <div className="flex flex-col w-full">
-                      <label className="ml-16 mb-1 mt-3 text-md font-semibold">
-                        Attach Relevant Image (If Any):
-                      </label>
-                      <input
-                        type="file"
-                        id="image"
-                        accept="image/*"
-                        onChange={handleImageChange}
-                        className="mb-3 ml-16"
-                      />
-                    </div>
-                    <button
-                      className="mt-4 mb-4 font-bold px-4 py-2 rounded-full bg-black hover:bg-teal-400 hover:text-black text-white inline-block w-1/4"
-                      type="submit"
-                    >
-                      Post
-                    </button>
-                  </form>
-                )}
+                  </div>
+                  <button
+                    className="mt-4 mb-4 font-bold px-4 py-2 rounded-full bg-gray-700 hover:bg-teal-400 hover:text-black text-white inline-block w-1/4"
+                    type="submit"
+                    onClick={() => handlePostSubmit(selectedCommunity)}
+                  >
+                    Post
+                  </button>
+                </form>
               </div>
-              {selectedCommunity !== "" ?
+              {selectedCommunity !== "" ? (
                 <h3 className="text-xl font-bold text-center mt-10">
                   See Other Posts from {selectedCommunity.name}
                 </h3>
-
-                : <></>}
+              ) : (
+                <></>
+              )}
 
               <div className="flex flex-col mx-4 w-full p-4">
-                {posts &&
-                  posts.length !== 0 ? (
-                  posts
-                    
-                    .map((post, index) => (
-                      <div key={index} className="">
-                        <Post
-                          community={selectedCommunity.name}
-                          user={post.author}
-                          text={post.content}
-                          createdAt={post.createdAt}
-                          likes={post.likes}
-                          comments={post.comments}
-                        />
-                      </div>
-                    ))
+                {posts && posts.length !== 0 ? (
+                  posts.map((post, index) => (
+                    <div key={index} className="">
+                      <Post
+                        community={selectedCommunity.name}
+                        user={post.author}
+                        text={post.content}
+                        createdAt={post.createdAt}
+                        likes={post.likes}
+                        comments={post.comments}
+                      />
+                    </div>
+                  ))
                 ) : (
                   <h3 className="text-xl font-bold text-center mt-10">
                     No posts available
@@ -289,11 +280,12 @@ const Community = () => {
                           Join
                         </button>
                       </div>
-                    ))) :
-                    <h3 className="text-xl font-bold text-center mt-10">
-                      You have joined all communities
+                    ))
+                  ) : (
+                    <h3 className="text-base font-bold text-center mt-10">
+                      Congratulations! You have joined All Available Communities
                     </h3>
-                  }
+                  )}
                 </div>
               </div>
             </div>
